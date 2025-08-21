4109ad56aeb8955842e8deb522dadc569ed82dcd
Gabriele Pintus <gabrielegpintus@gmail.com>
2025-08-21 15:28:57 +0200

    commented DialogHandler class

--- app/src/main/java/org/boardgames/crossway/controller/DialogHandler.java
+++ app/src/main/java/org/boardgames/crossway/controller/DialogHandler.java
@@ -2,16 +2,38 @@ package org.boardgames.crossway.controller;
 
 import javax.swing.*;
 
+/**
+ * A utility class for displaying various types of dialogs to the user.
+ * <p>
+ * This class centralizes the logic for showing warning, informational, and error dialogs,
+ * ensuring a consistent user experience. It requires a reference to the parent {@link JFrame}
+ * to correctly position the dialogs.
+ * </p>
+ *
+ * @author Gabriele Pintus
+ */
 public class DialogHandler {
 
+    /**
+     * The parent frame for the dialogs, used for correct positioning.
+     */
     private final JFrame frame;
 
+    /**
+     * Constructs a new DialogHandler.
+     *
+     * @param frame The parent {@link JFrame} to which dialogs will be attached.
+     */
     public DialogHandler(JFrame frame) {
         this.frame = frame;
     }
 
     /**
      * Displays a warning dialog to the user.
+     * <p>
+     * This method shows a standard Swing warning message dialog with a yellow
+     * triangle icon.
+     * </p>
      *
      * @param title   The title of the dialog.
      * @param message The message to display.
@@ -22,6 +44,10 @@ public class DialogHandler {
 
     /**
      * Displays an informational dialog to the user.
+     * <p>
+     * This method shows a standard Swing informational dialog, typically with
+     * an 'i' icon.
+     * </p>
      *
      * @param title   The title of the dialog.
      * @param message The message to display.
@@ -32,6 +58,10 @@ public class DialogHandler {
 
     /**
      * Displays an error dialog to the user.
+     * <p>
+     * This method shows a standard Swing error message dialog, typically with
+     * a red 'X' icon.
+     * </p>
      *
      * @param title   The title of the dialog.
      * @param message The message to display.
